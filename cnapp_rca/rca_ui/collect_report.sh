60e9bf6fe2865e05fb6e9b86463d8c5f70614ae8
Sam <vuillaumes@gmail.com>
2026-08-12 10:50:55 -0400

    Update collect_report.sh

--- cnapp_rca/rca_ui/collect_report.sh
+++ cnapp_rca/rca_ui/collect_report.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-set -e
-
-docker cp rca:/app/contacts.csv ./contacts.csv
-
-echo "Reports collected:"
-ls -lh ./contacts.csv
+#!/bin/sh
+echo "Fetching Contacts"
+sleep 1
+sudo docker cp rca:/app/contacts.csv ./contacts.csv
+sleep 1
+echo "Here is the list of contacts:"
+cat contacts.csv
